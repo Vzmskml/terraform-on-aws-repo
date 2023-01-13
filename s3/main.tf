f32694f58037e76f238feec61239a02dbb60b372
Vzmskml <118874695+Vzmskml@users.noreply.github.com>
2023-01-13 11:08:45 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,9 +1,8 @@
 provider "aws" {
-  profile = "SKMLVZM_TERRAFORM"
-  region = us-east-2
+  region = var.aws_region
 }
 
-resource "aws_s3_bucket1" "my-s3-bucket1" {
+resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
   
